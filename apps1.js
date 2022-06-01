4bfcbf69f40f310feddea7732d9f2ab8c2695496
Mick <ingenious90@mail.ru>
2022-06-01 16:04:49 +0300

    homework3

---
+++ apps1.js
@@ -0,0 +1,10 @@
+'use strict'
+for (let i = 0; i <= 10; i++) {
+    if (i === 0) {
+        console.log(i + ' Это ноль');
+    } else if (i % 2 != 0) {
+        console.log(i + ' Нечетное число')
+    } else {
+        console.log(i + ' Число четное')
+    }
+}
